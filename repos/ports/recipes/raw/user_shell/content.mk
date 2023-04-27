9d811cf9d70c7da8dd3888a64bdb2abf8c394782
devashish0007 <devashish.dewangan@ssipmt.com>
2023-04-27 11:33:39 +0530

    Initial commit

---
+++ repos/ports/recipes/raw/user_shell/content.mk
@@ -0,0 +1,4 @@
+content: user_shell.config
+
+user_shell.config:
+	cp $(REP_DIR)/recipes/raw/user_shell/$@ $@
--- repos/ports/src/noux-pkg/tar/target.mk
+++ repos/ports/src/noux-pkg/tar/target.mk
@@ -1,1 +1,3 @@
+ CONFIGURE_ARGS = --bindir=/bin \
+                  --libexecdir=/libexec
 include $(call select_from_repositories,mk/noux.mk)
